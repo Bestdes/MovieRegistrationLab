0af93ea225fbafa7d96a19342376ced996ad992c
Shawn <deshawn.dyson.career@gmail.com>
2020-05-21 07:55:32 -0400

    This is the initial commit

---
+++ MovieRegistration/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRegistration.Models
+{
+    public class Movie
+    {
+
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Genre { get; set; }
+
+        public int Year { get; set; }
+
+        public List<string> Actors { get; set; }
+
+        public List<string> Directors { get; set; }
+    }
+}
